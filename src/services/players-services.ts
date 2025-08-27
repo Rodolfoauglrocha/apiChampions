338f97efc3e8e31af2b4e7b67d790ebf91e626b6
Rodolfo Rocha <rodolfo.rocha@ccc.ufcg.edu.br>
2025-08-26 21:55:34 -0300

    Segunda versão

--- src/services/players-services.ts
+++ src/services/players-services.ts
@@ -1,8 +1,9 @@
+import * as PlayerRepository from "../repositories/player-repository"
 import { noContent, ok } from "../utils/http-helper"
 
 export const getPlayerData = async()=> {
     
-    const data = { player: "Ronaldo"}
+    const data = await PlayerRepository.findAllPlayers()
 
     let response = null
 
